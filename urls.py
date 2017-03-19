144465e8215b13b15d7631c414b5533f60f51976
Jasying <1378694610@qq.com>
2017-03-19 12:54:57 +1100

    Add files via upload
    These are Django code. The first 'urls.py' is under the file 'my-project'. The second 'urls.py' and 'views.py' is under the file 'posts'. I use the 'my-project'-'urls.py' to include my new url and approach 'views.py' from the new url file (second 'urls.py'). It seems like that I use the wrong way to 'import' them. I change 'import . from views' to 'import views from *'. However, it does not work and it says that 'views' is not a name.

---
+++ urls.py
@@ -0,0 +1,31 @@
+"""my_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import  url, include
+# include means that you want to include your new url
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^posts/', include('posts.urls')),
+    #url(string,include())
+    #r represents string
+    # ^ means if you match up to this point then you can look for posts
+    # ^ = http://127.0.0.1:8000/
+    # $ means end
+    # in 'posts', we can find another url, inside that url, we can find the view
+]
