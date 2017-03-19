be2bc7ac298ed373eb90d1c040940325e8714c28
Jasying <1378694610@qq.com>
2017-03-19 12:57:04 +1100

    Update views.py

--- views.py
+++ views.py
@@ -2,6 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def post_home(request):
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
 
-	return HttpResponse("<h1>Hello</h1>")
+def post_detail(request):
+	return HttpResponse("<h1>Detail</h1>")
+
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+	
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
